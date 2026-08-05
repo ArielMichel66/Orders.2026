dfab01010e7ea5a326d1d3d23ffe89e7dbc620bd
Ariel Quintana <aquintana@fanki.co>
2026-08-05 15:12:13 -0300

    add security frontend

---
+++ Orders/Orders.Frontend/AuthenticationProviders/AuthenticationProviderTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Orders.Frontend.AuthenticationProviders;
+
+public class AuthenticationProviderTest : AuthenticationStateProvider
+{
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        //await Task.Delay(1000); //esto es para probar q funciona la autenticacion
+        var anonimous = new ClaimsIdentity();
+        var user = new ClaimsIdentity(authenticationType: "test");
+        var admin = new ClaimsIdentity(
+                    [
+                        new("FirstName", "Ariel"),
+                        new("LastName", "Quintana"),
+                        new(ClaimTypes.Name, "ariel@yopmail.com"),
+                        new(ClaimTypes.Role, "Admin")
+                    ],
+                    authenticationType: "test");
+
+        return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(user)));
+    }
+}
