01bb1e266f993b1fb4aacc747865773ad819867f
Ariel Quintana <aquintana@fanki.co>
2026-08-12 15:12:12 -0300

    resend token

--- Orders/Orders.Backend/Controllers/AccountsController.cs
+++ Orders/Orders.Backend/Controllers/AccountsController.cs
@@ -213,4 +213,22 @@ public class AccountsController : ControllerBase
 
         return NoContent();
     }
+
+    [HttpPost("ResendToken")]
+    public async Task<IActionResult> ResedTokenAsync([FromBody] EmailDTO model)
+    {
+        var user = await _usersUnitOfWork.GetUserAsync(model.Email);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var response = await SendConfirmationEmailAsync(user);
+        if (response.WasSuccess)
+        {
+            return NoContent();
+        }
+
+        return BadRequest(response.Message);
+    }
 }
